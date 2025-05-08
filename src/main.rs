9c06d2ecd165921f4f312d831f132a80f0d2ab22
kian_Ahmadi <otyyjhct@gmail.com>
2025-05-08 18:21:26 +0330

    solveing the same problem with borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
-    let s = String::from("it is kian");
-    let (s2, len) = calculate_length(s);
-    println!("the length of {s2} is {len}");
+    let s1 = String::from("it is kian");
+    let len = calculate_length(&s1);
+    println!("the length of {s1} is {len}");
 }
 
-fn calculate_length(input: String) -> (String, usize) {
-    let length = input.len();
-    (input, length)
+fn calculate_length(input: &String) -> usize {
+    input.len()
 }
