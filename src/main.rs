8373716e32615688ac4cf553466c033a56d68707
kian_Ahmadi <otyyjhct@gmail.com>
2025-05-09 09:40:21 +0330

    mutable referncess

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let s1 = String::from("it is kian");
-    let len = calculate_length(&s1);
-    println!("the length of {s1} is {len}");
+    let mut s = String::from("hello");
+
+    change(&mut s);
 }
 
-fn calculate_length(input: &String) -> usize {
-    input.len()
+fn change(some_string: &mut String) {
+   some_string.push_str(", kian");
 }
