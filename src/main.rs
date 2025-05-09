9f5c1e0c9d37b83780a94cdee6d266617b6b8b73
kian_Ahmadi <otyyjhct@gmail.com>
2025-05-09 10:41:53 +0330

    fixing the multible combined reference problem

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ fn main() {
 
     let r1 = &s; // no problem
     let r2 = &s; // no problem
+
+    println!("{r1}");
+    println!("{r2}");
+    
     let r3 = &mut s; // BIG PROBLEM
 
-    println!("{}, {}, and {}", r1, r2, r3);
+    println!("{r3}");
 }
