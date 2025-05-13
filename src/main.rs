3ba4b65c83cdbc62a515df4596ecca4dfd3b14f8
kian_Ahmadi <otyyjhct@gmail.com>
2025-05-13 14:11:40 +0330

    learning about rust slicess an example case that findes the ences of spacess in strings

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
-fn main() {
-    let reference_to_nothing = dangle();
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
 }
 
-fn dangle() -> &String {
-    let s = String::from("hello");
+fn main() {
+    let mut s = String::from("hello world");
 
-    &s
+    let word = first_word(&s); // word will get the value 5
+
+    s.clear(); // this empties the String, making it equal to ""
+
+    // `word` still has the value `5` here, but `s` no longer has any content
+    // that we could meaningfully use with the value `5`, so `word` is now
+    // totally invalid!
+    println!("{s}");
+    println!("{word}")
 }
