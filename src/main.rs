8ace59f5f2b24a6677caf1e5a4df3622808e1fae
kian_Ahmadi <otyyjhct@gmail.com>
2025-06-07 16:23:37 +0330

    refactored the previous programe with structes to add more meaning

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+struct Rectangle {
+    width: u32,
+    hight: u32,
+}
+
 fn main() {
-    let rect1 = (30, 50);
+    let rect1 = Rectangle {
+        width: 30,
+        hight: 50,
+    };
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(rect1)
+        area(&rect1)
     );
 }
 
-fn area(dimentions: (u32, u32)) -> u32 {
-    dimentions.0 * dimentions.1
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.hight
 }
