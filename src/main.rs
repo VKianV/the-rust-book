0929a04b6794d6f25bb71b704931f7921ba11106
kian_Ahmadi <otyyjhct@gmail.com>
2025-06-07 19:24:02 +0330

    working with dbg! for debuging

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,15 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
-    hight: u32,
+    height: u32,
 }
 
 fn main() {
+    let scale = 2;
     let rect1 = Rectangle {
-        width: 30,
-        hight: 50,
+        width: dbg!(30 * scale),
+        height: 50,
     };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.hight
+    dbg!(&rect1);
 }
