ad52ec5021cafab998b9586c05cda04a4a5eeb15
kian_Ahmadi <otyyjhct@gmail.com>
2025-06-10 13:18:18 +0330

    a simple enum has defined

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,14 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width * self.height > other.width * other.height
-    }
+enum IpAddrKind {
+    V4,
+    V6,
 }
 
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
-    let rect3 = Rectangle {
-        width: 60,
-        height: 40,
-    };
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
 
-    println!(
-        "Can rect1 hold rect2? well the answare is {}",
-        rect1.can_hold(&rect2)
-    );
-    println!(
-        "Can rect1 hold rect3? well the answare is {}",
-        rect1.can_hold(&rect3)
-    );
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
 }
+
+fn route(ip_kind: IpAddrKind) {}
