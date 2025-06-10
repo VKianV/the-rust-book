f655307355bc8bb7ed5a1a44be7aa96f9730d26e
kian_Ahmadi <otyyjhct@gmail.com>
2025-06-10 19:14:57 +0330

    a simple app created for figuring out how does pattern matching works in rust

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-enum IpAddrKind {
-    V4,
-    V6,
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
 }
 
-fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
-
-    route(IpAddrKind::V4);
-    route(IpAddrKind::V6);
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
 }
 
-fn route(ip_kind: IpAddrKind) {}
+fn main() {}
