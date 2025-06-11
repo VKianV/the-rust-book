5ff74ced049279bf5eea3a6d00c4dbc4793767b9
kian_Ahmadi <otyyjhct@gmail.com>
2025-06-11 08:50:48 +0330

    a simple linked list was writen. this implamantion doesn't have a loop

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter,
+struct Node {
+    current: i32,
+    next: Option<Box<Node>>,
 }
 
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => 1,
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter => 25,
-    }
+fn main() {
+    let node3 = Node {
+        current: 3,
+        next: None,
+    };
+    let node2 = Node {
+        current: 2,
+        next: Some(Box::new(node3)),
+    };
+    let node1 = Node {
+        current: 1,
+        next: Some(Box::new(node2)),
+    };
 }
-
-fn main() {}
