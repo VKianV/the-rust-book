446ad79753cbcf582c6fc30954d7effbf9b450f8
kian_Ahmadi <otyyjhct@gmail.com>
2025-06-13 15:07:50 +0330

    an learning example with if let has been ilestrated

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,6 @@
-struct Node {
-    current: i32,
-    next: Option<Box<Node>>,
-}
-
 fn main() {
-    let node3 = Node {
-        current: 3,
-        next: None,
-    };
-    let node2 = Node {
-        current: 2,
-        next: Some(Box::new(node3)),
-    };
-    let node1 = Node {
-        current: 1,
-        next: Some(Box::new(node2)),
-    };
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("the maximum is configured to be {max}");
+    }
 }
