30c523a9d8653901edcf05b611ef4aa45fdae784
kian_Ahmadi <otyyjhct@gmail.com>
2025-06-19 03:07:15 +0330

    trying to itterate through a hashmap

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,11 @@ fn main() {
 
     scores.insert("Blue".to_string(), 10);
     scores.insert("Red".to_string(), 60);
+
+    // let team = "Blue".to_string();
+    // scores.get(&team).copied().unwrap_or(0);
+
+    for (key, value) in &scores {
+        println!("the team {}'s score is {}", key, value);
+    }
 }
