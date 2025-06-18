9e1d88240645861f5921ccbabbe1ee7ca968c582
kian_Ahmadi <otyyjhct@gmail.com>
2025-06-18 22:39:02 +0330

    Defining an enum to store values of different types in one vector

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+#[derive(Debug)]
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
+
 fn main() {
-    let mut v = vec![100, 32, 57];
-    for i in &mut v {
-        *i += 50;
+    let number = SpreadsheetCell::Int(3);
+    let text = SpreadsheetCell::Text(String::from("blue"));
+    let float = SpreadsheetCell::Float(10.12);
+
+    let row = vec![number, text, float];
+
+    for i in &row {
+        println!("{:?}", i);
     }
 }
