3dc8045b299ecd29d2e45b9c0d394cbba38c8a0f
kian_Ahmadi <otyyjhct@gmail.com>
2025-06-19 00:41:26 +0330

    extending it a little bit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
     let s1 = String::from("Hello, ");
     let s2 = String::from("world!");
-    let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
+    let s3 = String::from(" it is kian");
+    let s4 = s1 + &s2 + &s3; // note s1 has been moved here and can no longer be used
+
+    println!("{}", s4);
 }
