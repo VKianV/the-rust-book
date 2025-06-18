47347cafc75bbbbce4108e2e89f9b04c4d74e562
kian_Ahmadi <otyyjhct@gmail.com>
2025-06-18 18:51:05 +0330

    printed each element by vector by itereating over the elements useing a for loop

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 fn main() {
-    let v = vec![1, 2, 3];
-
-    let does_not_exist = &v[100];
-    println!("{does_not_exist}");
-
-    let does_not_exist = v.get(100);
-    println!("{does_not_exist:?}");
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{i}");
+    }
 }
