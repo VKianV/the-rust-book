95a276f013778c5579d5bbfae3c5080c2648d0b0
kian_Ahmadi <otyyjhct@gmail.com>
2025-06-19 00:55:52 +0330

    utilizing format macro for concatination

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let s1 = String::from("Hello, ");
-    let s2 = String::from("world!");
-    let s3 = String::from(" it is kian");
-    let s4 = s1 + &s2 + &s3; // note s1 has been moved here and can no longer be used
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
 
-    println!("{}", s4);
+    let s = format!("{}-{}-{}", &s1, &s2, &s3);
+    println!("{}", s);
 }
