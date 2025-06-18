53ae4408a8765241948dfbbb297d2beb42e3da12
kian_Ahmadi <otyyjhct@gmail.com>
2025-06-18 23:16:08 +0330

    Using the to_string method to create a String from a string literal

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,1 @@
-#[derive(Debug)]
-enum SpreadsheetCell {
-    Int(i32),
-    Float(f64),
-    Text(String),
-}
-
-fn main() {
-    let number = SpreadsheetCell::Int(3);
-    let text = SpreadsheetCell::Text(String::from("blue"));
-    let float = SpreadsheetCell::Float(10.12);
-
-    let row = vec![number, text, float];
-
-    for i in &row {
-        println!("{:?}", i);
-    }
-}
+fn main() {}
