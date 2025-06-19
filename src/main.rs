eb75d3c4c879f824bb2e241755bf84d842a0ad7d
kian_Ahmadi <otyyjhct@gmail.com>
2025-06-19 15:45:24 +0330

    causing a manual panic in rust

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-use std::collections::HashMap;
 fn main() {
-    let text = "By default, HashMap uses a hashing function called SipHash that can provide resistance to denial-of-service (DoS) attacks involving hash tables1.";
-    let mut map = HashMap::new();
-
-    for word in text.split_whitespace() {
-        let count = map.entry(word).or_insert(0);
-        *count += 1;
-    }
-
-    println!("{map:#?}")
+    panic!("there is no saveing Iran we have to evacuate");
 }
