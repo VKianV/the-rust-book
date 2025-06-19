0f1678b47878904b5ad200c89aad9609f734ffd5
kian_Ahmadi <otyyjhct@gmail.com>
2025-06-19 03:44:12 +0330

    Replacing a value stored with a particular key

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use std::collections::HashMap;
 fn main() {
-    let field_name = String::from("Favorite color");
-    let field_value = String::from("Blue");
+    let mut scores = HashMap::new();
 
-    let mut map = HashMap::new();
-    map.insert(&field_name, &field_value);
-    println!("name is: {}, value is {}", field_name, field_value);
-    println!("name is: {}, value is {}", field_name, field_value);
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+
+    println!("{scores:?}");
 }
