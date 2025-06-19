c81f6961b6787ae99465ba53b5546678c1ddc8c5
kian_Ahmadi <otyyjhct@gmail.com>
2025-06-19 03:34:33 +0330

    Showing that keys and values are owned by the hash map once they’re inserted

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 use std::collections::HashMap;
 fn main() {
-    let mut scores = HashMap::new();
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
 
-    scores.insert("Blue".to_string(), 10);
-    scores.insert("Red".to_string(), 60);
-
-    // let team = "Blue".to_string();
-    // scores.get(&team).copied().unwrap_or(0);
-
-    for (key, value) in &scores {
-        println!("the team {}'s score is {}", key, value);
-    }
+    let mut map = HashMap::new();
+    map.insert(&field_name, &field_value);
+    println!("name is: {}, value is {}", field_name, field_value);
+    println!("name is: {}, value is {}", field_name, field_value);
 }
