1c5a34806112e2779d99af5025bd928e9dafac44
kian_Ahmadi <otyyjhct@gmail.com>
2025-06-19 19:06:48 +0330

    Handling different kinds of errors in different ways

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::{fs::File, io::ErrorKind};
+
 fn main() {
-    panic!("there is no saveing Iran we have to evacuate");
+    let greeting_file_result = File::open("the_devil_file.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("the_devil_file.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("problem creating the file {e:?}"),
+            },
+            _ => {
+                panic!("error opening the file: {error:?}");
+            }
+        },
+    };
 }
