a7e3a2d521d341cae8fd7ea9fd177ec9948a80c5
kian_Ahmadi <otyyjhct@gmail.com>
2025-06-19 19:38:37 +0330

    while unwrap panics the thread expect method also gives a message about the context of the problem

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,6 @@
 use std::{fs::File, io::ErrorKind};
 
 fn main() {
-    let greeting_file_result = File::open("the_devil_file.txt");
-
-    let greeting_file = match greeting_file_result {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("the_devil_file.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("problem creating the file {e:?}"),
-            },
-            _ => {
-                panic!("error opening the file: {error:?}");
-            }
-        },
-    };
+    let greeting_file =
+        File::open("hello.txt").expect("the file must be included in the application");
 }
