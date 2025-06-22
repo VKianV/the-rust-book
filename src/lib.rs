4aeed223cba1631c523fc07873a009d1d57c40b7
kian_Ahmadi <otyyjhct@gmail.com>
2025-06-23 01:01:55 +0330

    implementing x and y points uesing genrics although i had problems with implmenting two diffrent types and referenceing a generic

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,30 @@
 use std::{any::type_name, fmt::Debug};
 
-pub struct Point<G1, G2> {
-    pub x: G1,
-    pub y: G2,
+#[derive(Debug)]
+pub struct Point<G> {
+    x: G,
+    y: G,
+}
+
+impl<G: Clone> Point<G> {
+    pub fn new(x: G, y: G) -> Self {
+        Self { x, y }
+    }
+
+    pub fn sauere(size: G) -> Self {
+        Self {
+            x: size.clone(),
+            y: size,
+        }
+    }
+
+    pub fn x(&self) -> &G {
+        &self.x
+    }
+
+    pub fn y(&self) -> &G {
+        &self.y
+    }
 }
 
 pub fn debug<G: Debug>(value: G) {
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use the_book::{Point, debug};
 
 fn main() {
-    let both_integer = Point { x: 5, y: 10 };
-    let both_float = Point { x: 1.0, y: 4.0 };
-    let integer_and_float = Point { x: 5, y: 4.0 };
+    // let p = Point::new(5, 10);
+    // let squre = Point::sauere(10);
+    // println!("x is {}, y is {}", &p.x(), &p.y());
+    // println!("the squre is {:#?}", squre);
 }
