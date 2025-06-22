c3a400a2cf257d88c136057bf20a8e8e56a03186
kian_Ahmadi <otyyjhct@gmail.com>
2025-06-22 06:30:18 +0330

    the simplist way to read a file

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,5 @@
-use std::{
-    fs::File,
-    io::{self, Read},
-};
+use std::{fs::read_to_string, io};
 
 pub fn read_username_from_file() -> Result<String, io::Error> {
-    let username_file_result = File::open("user_file.txt");
-    let mut username_file = match username_file_result {
-        Ok(file) => file,
-        Err(e) => return Err(e),
-    };
-    let mut username = String::new();
-
-    match username_file.read_to_string(&mut username) {
-        Ok(_) => Ok(username),
-        Err(e) => Err(e),
-    }
+    read_to_string("user_file.txt")
 }
