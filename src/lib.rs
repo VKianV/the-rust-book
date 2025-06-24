3c1eda6ce34809e2135a0f3f8ff5b13064cfa698
kian_Ahmadi <otyyjhct@gmail.com>
2025-06-24 13:37:54 +0330

    attempting to use a reference whose value has gone out of scope

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,5 @@
 use std::{any::type_name, fmt::Debug};
-
-#[derive(Debug)]
-pub struct Point<G> {
-    x: G,
-    y: G,
-}
-
-impl<G: Clone> Point<G> {
-    pub fn new(x: G, y: G) -> Self {
-        Self { x, y }
-    }
-
-    pub fn sauere(size: G) -> Self {
-        Self {
-            x: size.clone(),
-            y: size,
-        }
-    }
-
-    pub fn x(&self) -> &G {
-        &self.x
-    }
-
-    pub fn y(&self) -> &G {
-        &self.y
-    }
-}
-
-pub fn debug<G: Debug>(value: G) {
+pub fn debug<G>(value: &impl Debug) {
     println!(
         "the value is {:#?}. The type is: {}",
         value,
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use the_book::{Point, debug};
-
 fn main() {
-    // let p = Point::new(5, 10);
-    // let squre = Point::sauere(10);
-    // println!("x is {}, y is {}", &p.x(), &p.y());
-    // println!("the squre is {:#?}", squre);
+    let r;
+
+    {
+        let x = 5;
+        r = &x;
+    }
+
+    println!("r: {r}");
 }
