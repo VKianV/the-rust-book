266c7be66f6eac55226bdbb5bd74bd6aa5809534
kian_Ahmadi <otyyjhct@gmail.com>
2025-07-07 11:12:03 +0330

    learning how to work with testing in rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,24 @@
 use std::{any::type_name, fmt::Debug};
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
 pub fn debug<G>(value: &impl Debug) {
     println!(
         "the value is {:#?}. The type is: {}",
         value,
         type_name::<G>()
     );
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn explorations() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
 }
