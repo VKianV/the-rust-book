e19bf63461b2222e114a3f6d943974d54d7b9ffd
Kian Ahmadi <otyyjhct@gmail.com>
2025-07-31 20:58:50 +0330

    the Rectangle struct and its can_hold method from Chapter 5 for testing

--- src/lib.rs
+++ src/lib.rs
@@ -7,23 +7,14 @@ pub fn debug<G>(value: &impl Debug) {
         type_name::<G>()
     );
 }
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn explorations() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn another() {
-        panic!("Make this test fail");
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
