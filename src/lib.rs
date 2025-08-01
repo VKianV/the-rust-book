dd22d99050fdf414a918df4a61fccc65a7bd10f8
Kian Ahmadi <otyyjhct@gmail.com>
2025-08-01 14:45:24 +0330

    Testing for a panic! with a panic message containing a specified substring

--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,22 @@ pub fn debug<G>(value: &impl Debug) {
         type_name::<G>()
     );
 }
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
+
+pub struct Guess {
+    value: i32,
 }
 
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width < other.width && self.height > other.height
+// --snip--
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!("Guess value must be greater than or equal to 1, got {value}.");
+        } else if value > 100 {
+            panic!("Guess value must be less than or equal to 100, got {value}.");
+        }
+
+        Guess { value }
     }
 }
 
@@ -24,30 +31,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-
-        assert!(larger.can_hold(&smaller));
-    }
-
-    #[test]
-    fn smaller_cannot_hold_larger() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-
-        assert!(!smaller.can_hold(&larger));
+    #[should_panic(expected = "less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
