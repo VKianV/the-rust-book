fc9aed0c3e4c36425485ed93bc9a1565221ccbd0
Kian Ahmadi <otyyjhct@gmail.com>
2025-08-02 14:32:58 +0330

    Three tests with three different names

--- src/lib.rs
+++ src/lib.rs
@@ -12,18 +12,30 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+pub fn add_two(a: usize) -> usize {
+    a + 2
+}
+
 #[cfg(test)]
-mod tests {
+mod love {
     use super::*;
 
     #[test]
-    fn it_works() -> Result<(), String> {
-        let result = add(2, 2);
+    fn add_two_and_two() {
+        let result = add_two(2);
+        assert_eq!(result, 4);
+    }
 
-        if result == 4 {
-            Ok(())
-        } else {
-            Err(String::from("two plus two does not equal four"))
-        }
+    #[test]
+    #[ignore = "because it is slow to run"]
+    fn add_three_and_two() {
+        let result = add_two(3);
+        assert_eq!(result, 5);
+    }
+
+    #[test]
+    fn one_hundred() {
+        let result = add_two(100);
+        assert_eq!(result, 102);
     }
 }
