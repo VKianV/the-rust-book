dae3354dca068b31ab57eaa70e162e58808beea7
Kian Ahmadi <otyyjhct@gmail.com>
2025-08-08 17:19:44 +0330

    Shirt company giveaway situation

--- src/lib.rs
+++ src/lib.rs
@@ -7,35 +7,3 @@ pub fn debug<G>(value: &impl Debug) {
         type_name::<G>()
     );
 }
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-pub fn add_two(a: usize) -> usize {
-    a + 2
-}
-
-#[cfg(test)]
-mod love {
-    use super::*;
-
-    #[test]
-    fn add_two_and_two() {
-        let result = add_two(2);
-        assert_eq!(result, 4);
-    }
-
-    #[test]
-    #[ignore = "because it is slow to run"]
-    fn add_three_and_two() {
-        let result = add_two(3);
-        assert_eq!(result, 5);
-    }
-
-    #[test]
-    fn one_hundred() {
-        let result = add_two(100);
-        assert_eq!(result, 102);
-    }
-}
