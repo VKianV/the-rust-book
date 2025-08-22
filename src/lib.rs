f79414d540869657adc9988464dbeabb9fcf09ad
Kian Ahmadi <otyyjhct@gmail.com>
2025-08-22 18:06:27 +0330

    A documentation comment for a function

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,17 @@ pub fn debug<G>(value: &impl Debug) {
         type_name::<G>()
     );
 }
+
+/// Adds one to the number given.
+///
+/// # some Samples
+///
+/// ```
+/// let arg = 5;
+/// let answer = the_book::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 2
+}
