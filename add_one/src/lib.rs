a483cf0c22f9d276a63c43d4774a43a51b8fbd3b
Kian Ahmadi <otyyjhct@gmail.com>
2025-08-23 13:54:07 +0330

    ammend: doing some minor adjustments to wthe workspace

--- add_one/src/lib.rs
+++ add_one/src/lib.rs
@@ -1,5 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn add_two(number: i32) -> u64 {
+    number + 2
 }
 
 #[cfg(test)]
@@ -8,7 +8,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
+        let result = add_two(2);
         assert_eq!(result, 4);
     }
 }
