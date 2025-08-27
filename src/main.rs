000ec57b1790657ddbe2eb3d1aa3dc63e4ea2bd0
Kian Ahmadi <otyyjhct@gmail.com>
2025-08-27 11:54:27 +0330

    A library to keep track of how close a value is to a maximum value and warn when the value is at certain levels

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
-enum List {
-    Cons(i32, Rc<List>),
-    Nil,
+pub trait Messenger {
+    fn send(&self, msg: &str);
 }
 
-use crate::List::{Cons, Nil};
-use std::rc::Rc;
+pub struct LimitTracker<'a, T: Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
+}
 
-fn main() {
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("count after creating a = {}", Rc::strong_count(&a));
-    let b = Cons(3, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a));
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
+impl<'a, T> LimitTracker<'a, T>
+where
+    T: Messenger,
+{
+    pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max,
+        }
     }
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+
+        let percentage_of_max = self.value as f64 / self.max as f64;
+
+        if percentage_of_max >= 1.0 {
+            self.messenger.send("Error: You are over your quota!");
+        } else if percentage_of_max >= 0.9 {
+            self.messenger
+                .send("Urgent warning: You've used up over 90% of your quota!");
+        } else if percentage_of_max >= 0.75 {
+            self.messenger
+                .send("Warning: You've used up over 75% of your quota!");
+        }
+    }
 }
