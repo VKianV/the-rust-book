966584d47c5338e70d9cbd9f8b1b5dfc95b15eab
Kian Ahmadi <otyyjhct@gmail.com>
2025-09-01 09:27:38 +0330

    Creating a reference cycle of two List values pointing to each other

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,13 @@ use std::cell::RefCell;
 use std::rc::Rc;
 
 #[derive(Debug)]
-enum List {
+pub enum List {
     Cons(i32, RefCell<Rc<List>>),
     Nil,
 }
 
 impl List {
-    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+    pub fn tail(&self) -> Option<&RefCell<Rc<List>>> {
         match self {
             Cons(_, item) => Some(item),
             Nil => None,
