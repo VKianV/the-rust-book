4373ac78f25deea48e4d2301c771fa24c5a7d535
Kian Ahmadi <otyyjhct@gmail.com>
2025-09-01 20:01:24 +0330

    Creating a leaf node with no children and a branch node with leaf as one of its children

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,8 @@
-use crate::List::{Cons, Nil};
 use std::cell::RefCell;
 use std::rc::Rc;
 
 #[derive(Debug)]
-pub enum List {
-    Cons(i32, RefCell<Rc<List>>),
-    Nil,
-}
-
-impl List {
-    pub fn tail(&self) -> Option<&RefCell<Rc<List>>> {
-        match self {
-            Cons(_, item) => Some(item),
-            Nil => None,
-        }
-    }
+pub struct Node {
+    pub value: i32,
+    pub children: RefCell<Vec<Rc<Node>>>,
 }
