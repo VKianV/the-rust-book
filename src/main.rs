ea9c236edca9588e7e79d3c5215533998aebb30e
Kian Ahmadi <otyyjhct@gmail.com>
2025-09-05 16:40:14 +0330

    simulating a dead lock in rust with mutexes

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,26 @@
-use std::{
-    sync::{Arc, Mutex},
-    thread,
-};
-
+use std::sync::{Arc, Mutex};
+use std::thread;
 fn main() {
-    let counter = Arc::new(Mutex::new(0));
-    let mut handles = vec![];
+    let mutex1 = Arc::new(Mutex::new(0));
+    let mutex2 = Arc::new(Mutex::new(0));
 
-    for _ in 0..10 {
-        let counter = Arc::clone(&counter);
-        let handle = thread::spawn(move || {
-            let mut num = counter.lock().unwrap();
+    let m1_clone = Arc::clone(&mutex1);
+    let m2_clone = Arc::clone(&mutex2);
 
-            *num += 1;
-        });
-        handles.push(handle);
-    }
+    let t1 = thread::spawn(move || {
+        let _guard1 = m1_clone.lock().unwrap(); // Lock mutex1
+        thread::sleep(std::time::Duration::from_millis(100)); // Simulate work
+        let _guard2 = m2_clone.lock().unwrap(); // Wait for mutex2
+        println!("Thread 1: Acquired both locks");
+    });
 
-    for handle in handles {
-        handle.join().unwrap();
-    }
+    let t2 = thread::spawn(move || {
+        let _guard2 = mutex2.lock().unwrap(); // Lock mutex2
+        thread::sleep(std::time::Duration::from_millis(100)); // Simulate work
+        let _guard1 = mutex1.lock().unwrap(); // Wait for mutex1
+        println!("Thread 2: Acquired both locks");
+    });
 
-    println!("Result: {}", *counter.lock().unwrap());
+    t1.join().unwrap();
+    t2.join().unwrap();
 }
