15b80d74a75b084eecdbeed3790c209f9fb7a746
Kian Ahmadi <otyyjhct@gmail.com>
2025-09-06 12:22:47 +0330

    Defining an async function to get the title element from an HTML page

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,11 @@
-use std::sync::{Arc, Mutex};
-use std::thread;
-fn main() {
-    let mutex1 = Arc::new(Mutex::new(0));
-    let mutex2 = Arc::new(Mutex::new(0));
+use trpl::Html;
 
-    let m1_clone = Arc::clone(&mutex1);
-    let m2_clone = Arc::clone(&mutex2);
+fn main() {}
 
-    let t1 = thread::spawn(move || {
-        let _guard1 = m1_clone.lock().unwrap(); // Lock mutex1
-        thread::sleep(std::time::Duration::from_millis(100)); // Simulate work
-        let _guard2 = m2_clone.lock().unwrap(); // Wait for mutex2
-        println!("Thread 1: Acquired both locks");
-    });
-
-    let t2 = thread::spawn(move || {
-        let _guard2 = mutex2.lock().unwrap(); // Lock mutex2
-        thread::sleep(std::time::Duration::from_millis(100)); // Simulate work
-        let _guard1 = mutex1.lock().unwrap(); // Wait for mutex1
-        println!("Thread 2: Acquired both locks");
-    });
-
-    t1.join().unwrap();
-    t2.join().unwrap();
+async fn page_title(url: &str) -> Option<String> {
+    let response = trpl::get(url).await;
+    let response_text = response.text().await;
+    Html::parse(&response_text)
+        .select_first("title")
+        .map(|title_element| title_element.inner_html())
 }
