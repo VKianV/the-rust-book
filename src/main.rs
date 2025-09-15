c25817c4a99dc900836585d38bd59b108ea042b6
Kian Ahmadi <otyyjhct@gmail.com>
2025-09-15 19:45:14 +0330

    A revision of the code from Listing 17-11 that correctly shuts down when complete

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,27 @@ fn main() {
     trpl::run(async {
         let (tx, mut rx) = trpl::channel();
 
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("future"),
-        ];
+        let tx_fut = async move {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("future"),
+            ];
 
-        for val in vals {
-            tx.send(val).unwrap();
-            trpl::sleep(Duration::from_millis(500)).await;
-        }
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
 
-        while let Some(value) = rx.recv().await {
-            println!("received '{value}'");
-        }
+        let rx_fut = async move {
+            while let Some(value) = rx.recv().await {
+                println!("received '{value}'");
+            }
+            rx.close();
+        };
+
+        trpl::join(tx_fut, rx_fut).await;
     });
 }
