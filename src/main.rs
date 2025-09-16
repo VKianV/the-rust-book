485bf6bf441ac7cbb0a6f4a4d4b83d6440b7cf7f
Kian Ahmadi <otyyjhct@gmail.com>
2025-09-16 15:43:26 +0330

    Using race to get the result of whichever future finishes first

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,19 @@
-use std::pin::{Pin, pin, time::Duration};
+use std::time::Duration;
 
 fn main() {
     trpl::run(async {
-        let (tx, mut rx) = trpl::channel();
+        let slow = async {
+            println!("'slow' started.");
+            trpl::sleep(Duration::from_millis(100)).await;
+            println!("'slow' finished.");
+        };
 
-        let tx1 = tx.clone();
-        let tx1_fut = pin!(async move {
-            let vals = vec![
-                String::from("hi"),
-                String::from("from"),
-                String::from("the"),
-                String::from("future"),
-            ];
+        let fast = async {
+            println!("'fast' started.");
+            trpl::sleep(Duration::from_millis(50)).await;
+            println!("'fast' finished.");
+        };
 
-            for val in vals {
-                tx1.send(val).unwrap();
-                trpl::sleep(Duration::from_secs(1)).await;
-            }
-        });
-
-        let rx_fut = pin!(async {
-            while let Some(value) = rx.recv().await {
-                println!("received '{value}'");
-            }
-        });
-
-        let tx_fut = pin!(async move {
-            let vals = vec![
-                String::from("more"),
-                String::from("messages"),
-                String::from("for"),
-                String::from("you"),
-            ];
-
-            for val in vals {
-                tx.send(val).unwrap();
-                trpl::sleep(Duration::from_secs(1)).await;
-            }
-        });
-
-        let futures: Vec<Pin<&mut dyn Future<Output = ()>>> = vec![tx1_fut, rx_fut, tx_fut];
-
-        trpl::join_all(futures).await;
+        trpl::race(slow, fast).await;
     });
 }
