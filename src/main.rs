cfa00c08134c4a6da1b5084b29259552af104b39
Kian Ahmadi <otyyjhct@gmail.com>
2025-09-17 14:17:52 +0330

    Comparing the performance of sleep and yield_now

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,33 @@
-use std::{thread, time::Duration};
+use std::time::{Duration, Instant};
 
 fn main() {
     trpl::run(async {
-        let a = async {
-            println!("'a' started.");
-            slow("a", 30);
-            trpl::yield_now().await;
-            slow("a", 10);
-            trpl::yield_now().await;
-            slow("a", 20);
-            trpl::yield_now().await;
-            println!("'a' finished.");
-        };
+        let one_ns = Duration::from_nanos(1);
 
-        let b = async {
-            println!("'b' started.");
-            slow("b", 75);
-            trpl::yield_now().await;
-            slow("b", 10);
-            trpl::yield_now().await;
-            slow("b", 15);
-            trpl::yield_now().await;
-            slow("b", 350);
-            trpl::yield_now().await;
-            println!("'b' finished.");
-        };
+        let start = Instant::now();
+        async {
+            for _ in 1..1000 {
+                trpl::sleep(one_ns).await;
+            }
+        }
+        .await;
+        let time = Instant::now() - start;
+        println!(
+            "'sleep' version finished after {} seconds.",
+            time.as_secs_f32()
+        );
 
-        trpl::race(a, b).await;
+        let start = Instant::now();
+        async {
+            for _ in 1..1000 {
+                trpl::yield_now().await;
+            }
+        }
+        .await;
+        let time = Instant::now() - start;
+        println!(
+            "'yield' version finished after {} seconds.",
+            time.as_secs_f32()
+        );
     });
-}
-
-fn slow(name: &str, ms: u64) {
-    thread::sleep(Duration::from_millis(ms));
-    println!("'{name}' ran for {ms}ms");
 }
