067ff79ca4378acd7cae0887f5364add0f2dbcc7
Kian Ahmadi <otyyjhct@gmail.com>
2025-09-19 13:05:07 +0330

    an implmentation a encapsulation in rust

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,15 @@
-extern crate trpl; // required for mdbook test
-
-use std::{pin::pin, time::Duration};
-
-use trpl::{ReceiverStream, Stream, StreamExt};
+use the_book::AveragedCollection;
 
 fn main() {
-    trpl::run(async {
-        let messages = get_messages().timeout(Duration::from_millis(200));
-        let intervals = get_intervals()
-            .map(|count| format!("Interval: {count}"))
-            .timeout(Duration::from_secs(10));
-        let merged = messages.merge(intervals);
-        let mut stream = pin!(merged);
+    let mut collection1 = AveragedCollection::new(50);
 
-        while let Some(result) = stream.next().await {
-            match result {
-                Ok(message) => println!("{message}"),
-                Err(reason) => eprintln!("Problem: {reason:?}"),
-            }
-        }
-    })
-}
+    collection1.add(40);
+    collection1.add(50);
+    collection1.add(60);
+    collection1.add(70);
+    collection1.add(80);
+    collection1.add(90);
 
-fn get_messages() -> impl Stream<Item = String> {
-    let (tx, rx) = trpl::channel();
-
-    trpl::spawn_task(async move {
-        let messages = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
-        for (index, message) in messages.into_iter().enumerate() {
-            let time_to_sleep = if index % 2 == 0 { 100 } else { 300 };
-            trpl::sleep(Duration::from_millis(time_to_sleep)).await;
-
-            tx.send(format!("Message: '{message}'")).unwrap();
-        }
-    });
-
-    ReceiverStream::new(rx)
-}
-
-fn get_intervals() -> impl Stream<Item = u32> {
-    let (tx, rx) = trpl::channel();
-
-    trpl::spawn_task(async move {
-        let mut count = 0;
-        loop {
-            trpl::yield_now().await;
-            count += 1;
-            tx.send(count).unwrap();
-        }
-    });
-
-    ReceiverStream::new(rx)
+    dbg!(&collection1);
+    dbg!(&collection1);
 }
