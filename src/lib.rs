a5c7b929eca45bf5efe40dd3f9c91a6e58fafa83
Kian Ahmadi <otyyjhct@gmail.com>
2025-09-19 13:16:47 +0330

    A run method on Screen that calls the draw method on each component

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,10 @@ pub trait Draw {
 pub struct Screen {
     pub components: Vec<Box<dyn Draw>>,
 }
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,3 @@
-use the_book::AveragedCollection;
+use the_book::{Draw, Screen};
 
-fn main() {
-    let mut collection1 = AveragedCollection::new(50);
-
-    collection1.add(40);
-    collection1.add(50);
-    collection1.add(60);
-    collection1.add(70);
-    collection1.add(80);
-    collection1.add(90);
-
-    dbg!(&collection1);
-    dbg!(&collection1);
-}
+fn main() {}
