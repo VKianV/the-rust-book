f60ffc24f29b28579d8801d4ed749287f9ee51c2
Kian Ahmadi <otyyjhct@gmail.com>
2025-09-20 13:36:52 +0330

    forgot to add something

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
-use the_book::{Draw, Screen};
+use the_book::{Button, Draw, Screen};
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+    }
+}
 
 fn main() {
     let screen = Screen {
