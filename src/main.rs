d699b67f4390807e83806cdd59125938c78333af
Kian Ahmadi <otyyjhct@gmail.com>
2025-09-24 21:58:59 +0330

    Definition of a Post struct and a new function that creates a new Post instance, a State trait, and a Draft struct

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,14 @@
-use the_rust_book::{Button, Draw, Screen};
-
-struct SelectBox {
-    width: u32,
-    height: u32,
-    options: Vec<String>,
-}
-
-impl Draw for SelectBox {
-    fn draw(&self) {
-        // code to actually draw a select box
-    }
-}
+use the_rust_book::Post;
 
 fn main() {
-    let screen = Screen {
-        components: vec![
-            Box::new(SelectBox {
-                width: 75,
-                height: 10,
-                options: vec![
-                    String::from("Yes"),
-                    String::from("Maybe"),
-                    String::from("No"),
-                ],
-            }),
-            Box::new(Button {
-                width: 50,
-                height: 10,
-                label: String::from("OK"),
-            }),
-        ],
-    };
+    let mut post = Post::new();
 
-    screen.run();
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
 }
