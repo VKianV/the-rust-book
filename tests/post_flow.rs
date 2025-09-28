f497fb64183cd5164ca883468a46521bd30a9e5f
Kian Ahmadi <otyyjhct@gmail.com>
2025-09-28 09:31:35 +0330

    Adding a placeholder implementation for the content method on Post that always returns an empty string slice

---
+++ tests/post_flow.rs
@@ -0,0 +1,16 @@
+use the_rust_book::Post;
+
+#[test]
+fn post_follows_state_pattern() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+}
+
