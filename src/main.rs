191f8fe5a9ed48f84d8253413f256bf86ea258fd
Kian Ahmadi <otyyjhct@gmail.com>
2025-09-29 19:53:06 +0330

    A Post with a content method and DraftPost without a content method

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,14 @@
-fn main() {}
+use the_rust_book::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+}
