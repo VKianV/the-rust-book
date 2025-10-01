0cf9cdb47e1c48845c059ccf63ad07a04935d334
Kian Ahmadi <otyyjhct@gmail.com>
2025-10-01 13:39:45 +0330

    Mixing if let, else if, else if let, and else

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-use the_rust_book::Post;
-
 fn main() {
-    let mut post = Post::new();
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
 
-    post.add_text("I ate a salad for lunch today");
-
-    let post = post.request_review();
-
-    let post = post.approve();
-
-    assert_eq!("I ate a salad for lunch today", post.content());
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color, {color}, as the background");
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    } else {
+        println!("Using blue as the background color");
+    }
 }
