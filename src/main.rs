a7420740327ba1f8ed3d47b842f5005af4c93030
Kian Ahmadi <otyyjhct@gmail.com>
2025-10-02 16:15:38 +0330

    A match expression with an arm that introduces a new variable which shadows an existing variable y

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
-    let some_option_value: Option<i32> = None;
-    let Some(x) = some_option_value;
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {y}"),
+        _ => println!("Default case, x = {x:?}"),
+    }
+
+    println!("at the end: x = {x:?}, y = {y}");
 }
