98146807988555d4a076101d856032e42d5cec68
Kian Ahmadi <otyyjhct@gmail.com>
2025-10-03 22:34:32 +0330

    Adding a match guard to a pattern

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,9 @@
-enum Color {
-    Rgb(i32, i32, i32),
-    Hsv(i32, i32, i32),
-}
-
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(Color),
-}
-
 fn main() {
-    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+    let num = Some(4);
 
-    match msg {
-        Message::ChangeColor(Color::Rgb(r, g, b)) => {
-            println!("Change color to red {r}, green {g}, and blue {b}");
-        }
-        Message::ChangeColor(Color::Hsv(h, s, v)) => {
-            println!("Change color to hue {h}, saturation {s}, value {v}");
-        }
-        _ => (),
+    match num {
+        Some(x) if x % 2 == 0 => println!("The number {x} is even"),
+        Some(x) => println!("The number {x} is odd"),
+        None => (),
     }
 }
