8b7b677d43e8c3bd6b79d2eb72740a001714e212
Kian Ahmadi <otyyjhct@gmail.com>
2025-10-03 23:24:28 +0330

    Using @ to bind to a value in a pattern while also testing it

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 fn main() {
-    let x = Some(5);
-    let y = 10;
-
-    match x {
-        Some(50) => println!("Got 50"),
-        Some(n) if n == y => println!("Matched, n = {n}"),
-        _ => println!("Default case, x = {x:?}"),
+    enum Message {
+        Hello { id: i32 },
     }
 
-    println!("at the end: x = {x:?}, y = {y}");
+    let msg = Message::Hello { id: 5 };
+
+    match msg {
+        Message::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {id_variable}"),
+        Message::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        Message::Hello { id } => println!("Found some other id: {id}"),
+    }
 }
