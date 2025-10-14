74ac57b6c6f417ff8924118f4059cd938d0ac85c
Kian Ahmadi <otyyjhct@gmail.com>
2025-10-14 16:45:49 +0330

    messing with cargo fix

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,4 @@
 fn main() {
-    enum Message {
-        Hello { id: i32 },
-    }
-
-    let msg = Message::Hello { id: 5 };
-
-    match msg {
-        Message::Hello {
-            id: id_variable @ 0..10,
-        } => println!("Found an id in range: {id_variable}"),
-        Message::Hello { id: 10..=20 } => println!("Found an id in another range"),
-        Message::Hello { id } => println!("Found some other id: {id}"),
-    }
+    let mut x = 42;
+    println!("{x}");
 }
