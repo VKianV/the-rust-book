0b84855bbe9ce52086eaf5dc92197fa8dc378c58
Kian Ahmadi <otyyjhct@gmail.com>
2025-10-16 15:07:52 +0330

    Creating a raw pointer to an arbitrary memory address

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
 fn main() {
-    let address = 0x012345usize;
-    let r = address as *const i32;
+    // let mut num = 5;
+
+    let mut num2 = 50;
+
+    // let r1 = &raw const num;
+    // let r2 = &raw mut num;
+
+    let r3 = &num2;
+    let r4 = &mut num2;
+
+    // unsafe {
+    //     println!("r1 is: {:?}", *
+    //
+    //     r1);
+    //     println!("r2 is: {:?}", *r2);
+    // }
+
+    println!("r3 is: {}", r3);
+    println!("r4 is: {}", r4);
 }
