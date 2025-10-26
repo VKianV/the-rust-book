6ca6627ce8bd27c6a5f88e7f8e99352a8120cdb1
Kian Ahmadi <otyyjhct@gmail.com>
2025-10-26 10:17:31 +0330

    Explicitly marking a function as safe within an unsafe extern block and calling it safely

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
+unsafe extern "C" {
+    safe fn abs(input: i32) -> i32;
+}
+
 fn main() {
-    use std::slice;
-
-    let address = 0x01234usize;
-    let r = address as *mut i32;
-
-    let values: &[i32] = unsafe { slice::from_raw_parts_mut(r, 10000) };
+    println!("Absolute value of -3 according to C: {}", abs(-3));
 }
