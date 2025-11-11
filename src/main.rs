e36d18a2607aa1360a383a304b4553c36a56fb28
Kian Ahmadi <otyyjhct@gmail.com>
2025-11-11 13:48:34 +0330

    Implementing the OutlinePrint trait that requires the functionality from Display

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
-trait Animal {
-    fn baby_name() -> String;
-}
+use std::fmt;
 
-struct Dog;
-
-impl Dog {
-    fn baby_name() -> String {
-        String::from("Spot")
+trait OutlinePrint: fmt::Display {
+    fn outline_print(&self) {
+        let output = self.to_string();
+        let len = output.len();
+        println!("{}", "*".repeat(len + 4));
+        println!("*{}*", " ".repeat(len + 2));
+        println!("* {output} *");
+        println!("*{}*", " ".repeat(len + 2));
+        println!("{}", "*".repeat(len + 4));
     }
 }
 
-impl Animal for Dog {
-    fn baby_name() -> String {
-        String::from("puppy")
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
     }
 }
+
+impl OutlinePrint for Point {}
 
 fn main() {
-    println!("A baby dog is called a {}", Dog::baby_name());
+    let p = Point { x: 1, y: 3 };
+    p.outline_print();
 }
