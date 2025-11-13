dea7d2d42f2ac5423985d6735425117996fdbd70
Kian Ahmadi <otyyjhct@gmail.com>
2025-11-13 18:43:38 +0330

    Creating a Wrapper type around Vec<String> to implement Display

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,14 @@
 use std::fmt;
 
-trait OutlinePrint: fmt::Display {
-    fn outline_print(&self) {
-        let output = self.to_string();
-        let len = output.len();
-        println!("{}", "*".repeat(len + 4));
-        println!("*{}*", " ".repeat(len + 2));
-        println!("* {output} *");
-        println!("*{}*", " ".repeat(len + 2));
-        println!("{}", "*".repeat(len + 4));
-    }
-}
+struct Wrapper(Vec<String>);
 
-struct Point {
-    x: i32,
-    y: i32,
-}
-
-impl fmt::Display for Point {
+impl fmt::Display for Wrapper {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {})", self.x, self.y)
+        write!(f, "[{}]", self.0.join(", "))
     }
 }
-
-impl OutlinePrint for Point {}
 
 fn main() {
-    let p = Point { x: 1, y: 3 };
-    p.outline_print();
+    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+    println!("w = {}", w);
 }
