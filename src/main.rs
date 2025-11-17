87ba25105191f9c73a61a8fe18eab9a581c331c4
Kian Ahmadi <otyyjhct@gmail.com>
2025-11-17 11:04:36 +0330

    Introducing a type alias, Thunk, to reduce repetition

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-use std::fmt;
-
-struct Wrapper(Vec<String>);
-
-impl fmt::Display for Wrapper {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "[{}]", self.0.join(", "))
-    }
-}
-
 fn main() {
-    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
-    println!("w = {}", w);
+    type Thunk = Box<dyn Fn() + Send + 'static>;
+
+    let f: Thunk = Box::new(|| println!("hi"));
+
+    fn takes_long_type(f: Thunk) {
+        // --snip--
+    }
+
+    fn returns_long_type() -> Thunk {
+        // --snip--
+    }
 }
