805f02f80df6d48d0670108f39d0a26475368ab8
Kian Ahmadi <otyyjhct@gmail.com>
2025-12-05 13:51:42 +0330

    this time i didin't make it a workspace and I was curious was igoing to feel like only useing cargo anew

--- hello_macro_derive/src/lib.rs
+++ hello_macro_derive/src/lib.rs
@@ -1,12 +1,14 @@
-use proc_macro::TokenStream;
-use quote::quote;
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
 
-#[proc_macro_derive(HelloMacro)]
-pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
-    // Construct a representation of Rust code as a syntax tree
-    // that we can manipulate.
-    let ast = syn::parse(input).unwrap();
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    // Build the trait implementation.
-    impl_hello_macro(&ast)
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
 }
